120164f95a92661c421d4ed9f43f6d6d18eb443a
zach0909 <30294473+zach0909@users.noreply.github.com>
2019-11-17 11:48:41 +0200

    Update button.jsx

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button = props => {
     const btn_classes = props.status === "off" ? "btn btn-lg btn-primary" : "btn btn-lg btn-danger";
-    const btn_text = props.status === "off" ? "STARTy" : "STOP";
+    const btn_text = props.status === "off" ? "START" : "STOP";
     return ( 
         <button onClick={props.onClick} className={btn_classes}>{btn_text}</button>
      );
